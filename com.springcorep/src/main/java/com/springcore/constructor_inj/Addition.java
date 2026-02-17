ceba2c73910c6e7f1da995a8f22aa40f4a03a6dc
omsolanki271 <omsolanki271@gmail.com>
2026-02-18 00:17:00 +0530

    Addtion contructor one

--- com.springcorep/src/main/java/com/springcore/constructor_inj/Addition.java
+++ com.springcorep/src/main/java/com/springcore/constructor_inj/Addition.java
@@ -3,23 +3,32 @@ package com.springcore.constructor_inj;
 public class Addition {
 	private int a;
 	private int b;
+
+	// move to first doulbe result call first here 
+	public Addition(double a,double b)
+	{
+		this.a = (int) a;
+		this.b = (int) b;
+		System.out.println("Constroctor call : double , double ");
+	}
 	
 	public Addition(int a, int b)
 	{
 		this.a = a;
-		this.a = b;
+		this.b = b;
 		System.out.println("Constroctor call : int , int ");
 	}
-	public Addition(double a,double b)
-	{
-		this.a = (int) a;
-		this.a = (int) b;
-		System.out.println("Constroctor call : double , double ");
-	}
 	
+	// all to compare first call string after call other double 
+	public Addition(String a,String b)
+	{
+		this.a = Integer.parseInt(a);
+		this.b = Integer.parseInt(b);
+		System.out.println("Constroctor call : string  , string ");
+	}
 	public void doSum()
 	{
-		System.out.println("Sum of = "+ (this.a + this.b));
+		System.out.println("Sum of = " +  (this.a + this.b));
 	}
 
 }
